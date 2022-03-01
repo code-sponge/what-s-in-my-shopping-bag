998d20a2e5b7f24a3a5ec26a36b5eadeb6adc350
code-sponge <spon9ec0de@gmail.com>
2022-03-01 15:33:10 +0900

    js: refactoring

--- main.js
+++ main.js
@@ -3,78 +3,13 @@ const list = document.querySelector('.itemlist');
 const place = document.querySelector('.footer__itemPlace');
 const price = document.querySelector('.footer__itemPrice');
 const itemName = document.querySelector('.footer__itemName');
-
-addBtn.addEventListener('click', makeNode);
-
-place.addEventListener('keypress', (event) => {
-  console.log('key');
-  if (event.keyCode === 13) {
-    makeNode();
-  }
-});
-price.addEventListener('keypress', (event) => {
-  if (event.keyCode === 13) {
-    makeNode();
-  }
-});
-itemName.addEventListener('keypress', (event) => {
-  if (event.keyCode === 13) {
-    makeNode();
-  }
-});
+const footer__left = document.querySelector('.footer__left');
 
 function makeNode() {
   const placeValue = place.value;
   const priceValue = price.value;
   const itemNameValue = itemName.value;
-
-  const item = createElement('li', 'item');
-  const checkbox = createElement('input', 'checkbox');
-  checkbox.setAttribute('type', 'checkbox');
-  const main__left = createElement('div', 'main__left');
-  const placeName = createElement('div', 'place');
-  const locationIcon = createElement('i', 'fa-solid fa-location-dot');
-  const item__desc = createElement('div', 'item__desc');
-  const desc__name = createElement('div', 'desc__name');
-  const desc__price = createElement('div', 'desc__price');
-  const wonSignIcon = createElement('i', 'fa-solid fa-won-sign');
-  const price__number = createElement('div', 'price__number');
-  const trashCanIcon = createElement('i', 'fa-solid fa-trash-can');
-
-  desc__price.appendChild(wonSignIcon);
-  desc__price.appendChild(price__number);
-  item__desc.appendChild(desc__name);
-  item__desc.appendChild(desc__price);
-  placeName.appendChild(locationIcon);
-  main__left.appendChild(placeName);
-  main__left.appendChild(item__desc);
-  item.appendChild(checkbox);
-  item.appendChild(main__left);
-  item.appendChild(trashCanIcon);
-  const placeText = document.createTextNode(placeValue);
-  const priceText = document.createTextNode(priceValue);
-  const itemNameText = document.createTextNode(itemNameValue);
-  placeName.appendChild(placeText);
-  desc__name.appendChild(itemNameText);
-  price__number.appendChild(priceText);
-
-  trashCanIcon.onclick = () => {
-    list.removeChild(item);
-  };
-
-  checkbox.onclick = () => {
-    let itemToToggle = item;
-    itemToToggle.classList.toggle('active');
-  };
-  requestAnimationFrame(() => {
-    item.scrollIntoView({ block: 'center' });
-  });
-
-  place.value = '';
-  price.value = '';
-  itemName.value = '';
-  place.focus();
-
+  const itemself = createNode(placeValue, priceValue, itemNameValue);
   if (!placeValue) {
     alert('please input a place.');
   } else if (!priceValue) {
@@ -82,12 +17,49 @@ function makeNode() {
   } else if (!itemNameValue) {
     alert('please input a item');
   } else {
-    list.appendChild(item);
+    list.appendChild(itemself);
   }
+
+  itemself.scrollIntoView({ block: 'center' });
+
+  place.value = '';
+  price.value = '';
+  itemName.value = '';
+  place.focus();
+}
+let id = 0;
+function createNode(placeValue, priceValue, itemNameValue) {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.setAttribute('data-target-id', id);
+  item.innerHTML = `
+      <input class="checkbox" type="checkbox" />
+      <div class="main__left">
+        <div class="place"><i class="fa-solid fa-location-dot"></i>${placeValue}</div>
+        <div class="item__desc">
+          <div class="desc__name">${itemNameValue}</div>
+          <div class="desc__price">
+            <i class="fa-solid fa-won-sign"></i>
+            <div class="price__number">${priceValue}</div>
+          </div>
+        </div>
+      </div>
+      <i class="fa-solid fa-trash-can" data-id="${id}"></i>`;
+  id++;
+  return item;
 }
 
-function createElement(tag, classname) {
-  const newNode = document.createElement(tag);
-  newNode.setAttribute('class', classname);
-  return newNode;
-}
+list.addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const tobeDeleted = document.querySelector(`.item[data-target-id="${id}"]`);
+    tobeDeleted.remove();
+  }
+});
+
+addBtn.addEventListener('click', makeNode);
+footer__left.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    makeNode();
+  }
+});
